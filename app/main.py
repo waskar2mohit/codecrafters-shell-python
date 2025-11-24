7c7372364910d7f370a355a2012cc233b3c7cd51
waskar2mohit <waskarmohit@gmail.com>
2025-11-24 10:44:25 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
+    command = input("myCommand>")
+    if command == "":
+        print(f"{command}command not found")
     pass
 
 

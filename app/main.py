bba539d0b8f1f782187e8c2b8e710961c258af88
waskar2mohit <waskarmohit@gmail.com>
2025-11-30 20:44:29 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ def main():
         if command.startswith("echo "):
             commandArr = shlex.split(command[5:])
             for c in commandArr:
-                print(c,end=' ') 
+                print(c,end='\n') 
             continue
 
         if command.startswith("type "):
